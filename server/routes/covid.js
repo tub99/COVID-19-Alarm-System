84eb280e120a8756c090aa33aaa21cc716597202
tub99 <ghoshsoumya001@gmail.com>
2020-03-29 20:47:15 +0530

    API done

---
+++ server/routes/covid.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+const StateMap = require("./../services/stateMap");
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  axios
+    .get("https://api.covid19india.org/data.json")
+    .then(function(response) {
+      // handle success
+      let stateList = response.data.statewise;
+      
+     const stateWiseData = StateMap.getStateList(stateList);
+      res.send(stateWiseData);
+    })
+    .catch(function(error) {
+      // handle error
+      res.error(error);
+    })
+ 
+});
+
+router.get("/delta", function(req, res, next) {
+  axios
+    .get("https://api.covid19india.org/data.json")
+    .then(function(response) {
+      // handle success
+      console.log(response.data)
+      let stateList = response.data.statewise;
+      
+     const deltaList = StateMap.getDeltaList(stateList);
+     console.log(delta.deltaMap);
+      res.send(deltaList);
+    })
+    .catch(function(error) {
+      // handle error
+      res.error(error);
+    })
+ 
+});
+
+module.exports = router;
